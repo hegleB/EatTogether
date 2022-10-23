d82d976895cb03acda81ae3467bd94ad7ba1c3ee
dltkd1395 <dltkd1395@gmail.com>
2022-10-23 23:23:33 +0900

    feat: keyboard event 추가

---
+++ presenation/src/main/java/com/qure/presenation/utils/KeyboardEvent.kt
@@ -0,0 +1,23 @@
+package com.qure.presenation.utils
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+class KeyboardEvent(val context : Context) {
+
+    fun openKeyboard() {
+        val imm: InputMethodManager =
+            context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(
+            InputMethodManager.SHOW_FORCED,
+            InputMethodManager.HIDE_IMPLICIT_ONLY
+        )
+    }
+
+
+    fun hideKeyboard() {
+        val immhide = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        immhide.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+    }
+}
