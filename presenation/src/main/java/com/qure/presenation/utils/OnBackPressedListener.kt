c0c138779414063edf7febcf26363332bf5ddc96
dltkd1395 <dltkd1395@gmail.com>
2022-10-23 23:24:00 +0900

    feat: back press event 추가

---
+++ presenation/src/main/java/com/qure/presenation/utils/OnBackPressedListener.kt
@@ -0,0 +1,28 @@
+package com.qure.presenation.utils
+
+import android.app.Activity
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+
+class OnBackPressedListener {
+
+    fun finish(requestActivity : FragmentActivity, activity: Activity) {
+        requestActivity.onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                activity?.finish()
+            }
+
+        })
+    }
+
+    fun back(requestActivity : FragmentActivity,  navController : NavController) {
+        requestActivity.onBackPressedDispatcher.addCallback(object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                navController.popBackStack()
+            }
+        })
+    }
+}
