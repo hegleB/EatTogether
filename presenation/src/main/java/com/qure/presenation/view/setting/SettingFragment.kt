2266e218ec5c7034c3c64d2b6565c23308b8f2b9
dltkd1395 <dltkd13956@naver.com>
2023-01-17 02:39:23 +0900

    fix : 로그아웃 후 다시 로그인 했을 때 바텀 네비게이션 설정 아이템을 클릭하면 설정 뷰가 안보이는 오류 수정

--- presenation/src/main/java/com/qure/presenation/view/setting/SettingFragment.kt
+++ presenation/src/main/java/com/qure/presenation/view/setting/SettingFragment.kt
@@ -3,9 +3,6 @@ package com.qure.presenation.view.setting
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -15,11 +12,8 @@ import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.quer.presenation.base.BaseFragment
 import com.qure.domain.model.User
 import com.qure.presenation.R
-import com.qure.presenation.databinding.FragmentPeopleBinding
-import com.qure.presenation.databinding.FragmentProfileSettingBinding
 import com.qure.presenation.view.MainActivity
 import com.qure.presenation.viewmodel.SettingViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -38,14 +32,12 @@ class SettingFragment : PreferenceFragmentCompat() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-
         val loginInfo : Preference? = findPreference("setting_login_info")
         val LogOutPreference: Preference? = findPreference("setting_logout")
         val messagePreference: SwitchPreference? = findPreference("setting_message")
         val vibrationPreference : CheckBoxPreference? = findPreference("setting_vibrate")
         val soundPreference : CheckBoxPreference? = findPreference("setting_sound")
         val uid = firebaseAuth.currentUser!!.uid
-
 
         firestore.collection("users").document(uid).get().addOnCompleteListener{
                 task->
@@ -54,7 +46,6 @@ class SettingFragment : PreferenceFragmentCompat() {
                 loginInfo!!.summary = email!!.userid
             }
         }
-
         if(messagePreference!!.isChecked){
             vibrationPreference!!.isEnabled = true
             soundPreference!!.isEnabled = true
@@ -68,25 +59,18 @@ class SettingFragment : PreferenceFragmentCompat() {
             Preference.OnPreferenceChangeListener {
             @RequiresApi(Build.VERSION_CODES.O)
             override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
-
                 val now = System.currentTimeMillis()
                 if (messagePreference.isChecked) {
                     firestore.collection("setting").document(uid).update("message",false)
                     firestore.collection("setting").document(uid).update("notification_time",now)
                     vibrationPreference!!.isEnabled = false
                     soundPreference!!.isEnabled = false
-
-
                 } else {
                     firestore.collection("setting").document(uid).update("message",true)
                     firestore.collection("setting").document(uid).update("notification_time",now)
                     vibrationPreference!!.isEnabled = true
                     soundPreference!!.isEnabled = true
-
-
                 }
-
-
                 return true
             }
 
@@ -123,11 +107,11 @@ class SettingFragment : PreferenceFragmentCompat() {
         LogOutPreference!!.setOnPreferenceClickListener(object :
             Preference.OnPreferenceClickListener {
             override fun onPreferenceClick(preference: Preference?): Boolean {
-                FirebaseAuth.getInstance().signOut()
-                findNavController().navigate(R.id.loginFragment)
-                return false
+                viewModel.clickLogout()
+                findNavController().navigate(SettingFragmentDirections.actionSettingFragmentToLoginFragment())
+                return true
             }
-        })
 
+        })
     }
 }
--- presenation/src/main/java/com/qure/presenation/viewmodel/SettingViewModel.kt
+++ presenation/src/main/java/com/qure/presenation/viewmodel/SettingViewModel.kt
@@ -1,8 +1,10 @@
 package com.qure.presenation.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.qure.presenation.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -11,6 +13,7 @@ class SettingViewModel @Inject constructor(
     private val auth: FirebaseAuth,
 ) : ViewModel() {
 
-    var isNotify = MutableLiveData<Boolean>()
-
+    fun clickLogout() {
+        auth.signOut()
+    }
 }
