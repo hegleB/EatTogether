c3c517db2e991ab96c17b281d4b7da734e8ab9e3
dltkd1395 <dltkd13956@naver.com>
2023-01-18 01:47:56 +0900

    refactor(SettingFragment) : 메서드 분리 및 상수화

--- presenation/src/main/java/com/qure/presenation/view/setting/SettingFragment.kt
+++ presenation/src/main/java/com/qure/presenation/view/setting/SettingFragment.kt
@@ -26,131 +26,98 @@ class SettingFragment : PreferenceFragmentCompat() {
 
     @Inject
     lateinit var firebaseAuth: FirebaseAuth
-    private val viewModel : SettingViewModel by viewModels()
+    private val viewModel: SettingViewModel by viewModels()
+    private var loginInfo: Preference? = null
+    private var LogOutPreference: Preference? = null
+    private var messagePreference: SwitchPreference? = null
+    private var vibrationPreference: CheckBoxPreference? = null
+    private var soundPreference: CheckBoxPreference? = null
+
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
-        val loginInfo : Preference? = findPreference("setting_login_info")
-        val LogOutPreference: Preference? = findPreference("setting_logout")
-        val messagePreference: SwitchPreference? = findPreference("setting_message")
-        val vibrationPreference : CheckBoxPreference? = findPreference("setting_vibrate")
-        val soundPreference : CheckBoxPreference? = findPreference("setting_sound")
+        loginInfo = findPreference(SETTING_LOGIN_INFO)
+        LogOutPreference = findPreference(SETTING_LOGOUT)
+        messagePreference = findPreference(SETTING_MESSAGE)
+        vibrationPreference = findPreference(SETTING_VIBRATE)
+        soundPreference = findPreference(SETTING_SOUND)
         val uid = firebaseAuth.currentUser?.uid ?: ""
 
-        setCurrentLoginInfo(uid, loginInfo)
-        setMessageClickListener(messagePreference, uid, vibrationPreference, soundPreference)
-        setVibrationClickListener(vibrationPreference, uid)
-        setSoundClickListener(soundPreference, uid)
-        setLogoutClickListener(LogOutPreference)
+        setInitVibrationAndSoundPreference()
+        setCurrentLoginInfo(uid)
+        setMessageClickListener(uid)
+        setVibrationClickListener(uid)
+        setSoundClickListener(uid)
+        setLogoutClickListener()
     }
 
-    private fun setCurrentLoginInfo(uid: String, loginInfo: Preference?) {
-        firestore.collection(USERS_COLLECTION_PATH).document(uid).get().addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                val email = task.result.toObject(User::class.java)
-                loginInfo!!.summary = email!!.userid
+    private fun setInitVibrationAndSoundPreference() {
+        vibrationPreference!!.isEnabled = messagePreference!!.isChecked
+        soundPreference!!.isEnabled = messagePreference!!.isChecked
+    }
+
+    private fun setCurrentLoginInfo(uid: String) {
+        firestore.collection(USERS_COLLECTION_PATH).document(uid).get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val email = task.result.toObject(User::class.java)
+                    loginInfo?.summary = email?.userid
+                }
             }
-        }
     }
 
-    private fun setMessageClickListener(
-        messagePreference: SwitchPreference?,
-        uid: String,
-        vibrationPreference: CheckBoxPreference?,
-        soundPreference: CheckBoxPreference?
-    ) {
-        messagePreference!!.setOnPreferenceChangeListener(object :
+    private fun setMessageClickListener(uid: String) {
+        messagePreference?.setOnPreferenceChangeListener(object :
             Preference.OnPreferenceChangeListener {
             @RequiresApi(Build.VERSION_CODES.O)
             override fun onPreferenceChange(preference: Preference?, newValue: Any?): Boolean {
                 val now = System.currentTimeMillis()
-                setMessagePreference(
-                    messagePreference,
-                    uid,
-                    now,
-                    vibrationPreference,
-                    soundPreference
-                )
+                setMessagePreference(uid, now)
                 return true
             }
         })
     }
 
-    private fun setMessagePreference(
-        messagePreference: SwitchPreference,
-        uid: String,
-        now: Long,
-        vibrationPreference: CheckBoxPreference?,
-        soundPreference: CheckBoxPreference?
-    ) {
-        if (messagePreference.isChecked) {
-            updateMessageNotification(uid, now)
-            vibrationPreference!!.isEnabled = false
-            soundPreference!!.isEnabled = false
-        } else {
-            vibrationPreference!!.isEnabled = true
-            soundPreference!!.isEnabled = true
-        }
+    private fun setMessagePreference(uid: String, now: Long) {
+        vibrationPreference?.isEnabled = messagePreference!!.isChecked.not()
+        soundPreference?.isEnabled = messagePreference!!.isChecked.not()
+        updateMessageNotification(uid, now)
     }
 
     private fun updateMessageNotification(uid: String, now: Long) {
-        firestore.collection(SETTING_COLLECTION_PATH).document(uid).update(MESSAGE_FIELD, false)
-        firestore.collection(SETTING_COLLECTION_PATH).document(uid).update(NOTIFICATION_TIME_FIELD, now)
+        with(firestore.collection(SETTING_COLLECTION_PATH).document(uid)) {
+            update(MESSAGE_FIELD, messagePreference?.isChecked?.not())
+            update(NOTIFICATION_TIME_FIELD, now)
+        }
     }
 
-    private fun setVibrationClickListener(
-        vibrationPreference: CheckBoxPreference?,
-        uid: String
-    ) {
-        vibrationPreference!!.setOnPreferenceClickListener(object :
+    private fun setVibrationClickListener(uid: String) {
+        vibrationPreference?.setOnPreferenceClickListener(object :
             Preference.OnPreferenceClickListener {
             override fun onPreferenceClick(preference: Preference?): Boolean {
-                setVibrationPrefernce(vibrationPreference, uid)
+                firestore.collection(SETTING_COLLECTION_PATH)
+                    .document(uid)
+                    .update(VIBRATION_FIELD, vibrationPreference?.isChecked)
                 return true
             }
         })
     }
 
-    private fun setVibrationPrefernce(
-        vibrationPreference: CheckBoxPreference?,
-        uid: String
-    ) {
-        if (vibrationPreference!!.isChecked) {
-            firestore.collection(SETTING_COLLECTION_PATH).document(uid).update(VIBRATION_FIELD, true)
-
-        } else {
-            firestore.collection(SETTING_COLLECTION_PATH).document(uid).update(VIBRATION_FIELD, false)
-
-        }
-    }
-
-    private fun setSoundClickListener(
-        soundPreference: CheckBoxPreference?,
-        uid: String
-    ) {
-        soundPreference!!.setOnPreferenceClickListener(object :
+    private fun setSoundClickListener(uid: String) {
+        soundPreference?.setOnPreferenceClickListener(object :
             Preference.OnPreferenceClickListener {
             override fun onPreferenceClick(preference: Preference?): Boolean {
-                setSoundPrefernce(soundPreference, uid)
+                firestore.collection(SETTING_COLLECTION_PATH)
+                    .document(uid)
+                    .update(SOUND_FIELD, soundPreference?.isChecked)
                 return true
             }
         })
     }
 
-    private fun setSoundPrefernce(
-        soundPreference: CheckBoxPreference?,
-        uid: String
-    ) {
-        if (soundPreference!!.isChecked) {
-            firestore.collection(SETTING_COLLECTION_PATH).document(uid).update(SOUND_FIELD, true)
-        } else {
-            firestore.collection(SETTING_COLLECTION_PATH).document(uid).update(SOUND_FIELD, false)
-        }
-    }
-
-    private fun setLogoutClickListener(LogOutPreference: Preference?) {
-        LogOutPreference!!.setOnPreferenceClickListener(object :
+    private fun setLogoutClickListener() {
+        LogOutPreference?.setOnPreferenceClickListener(object :
             Preference.OnPreferenceClickListener {
             override fun onPreferenceClick(preference: Preference?): Boolean {
                 viewModel.clickLogout()
@@ -158,5 +125,13 @@ class SettingFragment : PreferenceFragmentCompat() {
                 return true
             }
         })
+    }
+
+    companion object {
+        const val SETTING_LOGIN_INFO = "setting_login_info"
+        const val SETTING_LOGOUT = "setting_logout"
+        const val SETTING_MESSAGE = "setting_message"
+        const val SETTING_VIBRATE = "setting_vibrate"
+        const val SETTING_SOUND = "setting_sound"
     }
 }
