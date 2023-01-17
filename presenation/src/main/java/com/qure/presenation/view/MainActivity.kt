854cb4fb3f1c85029e03a7501d1b9ee40e688ea6
dltkd1395 <dltkd13956@naver.com>
2023-01-18 05:58:50 +0900

    fix : 읽지 않은 메시지 개수가 안보이는 오류 수정

--- presenation/src/main/java/com/qure/presenation/view/MainActivity.kt
+++ presenation/src/main/java/com/qure/presenation/view/MainActivity.kt
@@ -1,85 +1,23 @@
 package com.qure.presenation.view
 
-import android.util.Log
-import androidx.core.content.ContextCompat
-import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreException
 import com.quer.presenation.base.BaseActivity
-import com.qure.domain.model.ChatRoom
 import com.qure.presenation.R
 import com.qure.presenation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-
-    @Inject
-    lateinit var firebaseAuth: FirebaseAuth
-
-    @Inject
-    lateinit var firestore: FirebaseFirestore
-
     override fun init() {
-        try {
-            setBadge()
-        } catch (e: FirebaseFirestoreException) {
-            Log.d("TAG", "Firebase Error")
-        }
+        setupBottomNavMenu()
+    }
 
+    private fun setupBottomNavMenu() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        setupBottomNavMenu(navController)
-    }
-
-    private fun setupBottomNavMenu(navController: NavController) {
-        val bottomNav = binding.bottomNavigation
-        bottomNav.setupWithNavController(navController)
-    }
-
-    fun setBadge() {
-        var badge = binding.bottomNavigation.getOrCreateBadge(R.id.chatFragment)
-        badge.backgroundColor = ContextCompat.getColor(this@MainActivity, R.color.light_red)
-
-        val currentId = firebaseAuth.currentUser?.uid
-        if (currentId != null) {
-            firestore.collection("chatrooms").whereArrayContains("users", currentId!!)
-                .addSnapshotListener { snapshot, e ->
-                    if (e == null) {
-
-                    }
-                    try {
-                        var chatCount = snapshot!!.toObjects(ChatRoom::class.java)
-
-                        var count = 0
-                        for (i in chatCount) {
-                            count += i.unreadCount.get(currentId)!!
-                        }
-                        if (count == 0) {
-                            badge.isVisible = false
-                        } else {
-                            badge.isVisible = true
-                            if (count >= 999) {
-                                badge.number = 999
-                            } else {
-                                badge.number = count
-                            }
-
-                        }
-                    } catch (e: NullPointerException) {
-                        Log.d("TAG", "Null Exception")
-                    }
-
-                }
-
-
-        }
-
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
--- presenation/src/main/java/com/qure/presenation/view/people/PeopleFragment.kt
+++ presenation/src/main/java/com/qure/presenation/view/people/PeopleFragment.kt
@@ -1,8 +1,16 @@
 package com.qure.presenation.view.people
 
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.quer.presenation.base.BaseFragment
+import com.qure.domain.model.ChatRoom
+import com.qure.domain.utils.CHATROOMS_COLLECTION_PATH
+import com.qure.domain.utils.USERS_FIELD
 import com.qure.presenation.R
 import com.qure.presenation.adapter.PeopleAdapter
 import com.qure.presenation.databinding.FragmentPeopleBinding
@@ -10,6 +18,7 @@ import com.qure.presenation.utils.BottomNavigationEvent
 import com.qure.presenation.utils.OnBackPressedListener
 import com.qure.presenation.viewmodel.PeopleViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
@@ -17,6 +26,11 @@ class PeopleFragment : BaseFragment<FragmentPeopleBinding>(R.layout.fragment_peo
 
     private val peopleViewModel: PeopleViewModel by activityViewModels()
 
+    @Inject
+    lateinit var firestore: FirebaseFirestore
+
+    @Inject
+    lateinit var firebaseAuth: FirebaseAuth
     private val adapter: PeopleAdapter by lazy {
         PeopleAdapter({ moveToProfileFragment(it.uid) })
     }
@@ -28,6 +42,7 @@ class PeopleFragment : BaseFragment<FragmentPeopleBinding>(R.layout.fragment_peo
         initAdapter()
         initViewModel()
         OnBackPressedListener().finish(requireActivity(), requireActivity())
+        setBadge()
     }
 
 
@@ -89,5 +104,43 @@ class PeopleFragment : BaseFragment<FragmentPeopleBinding>(R.layout.fragment_peo
             uid
         )
         findNavController().navigate(direction)
+    }
+
+    private fun setBadge() {
+        val badge = activity?.findViewById<BottomNavigationView>(R.id.bottom_navigation)!!
+            .getOrCreateBadge(R.id.chatFragment)
+        badge.backgroundColor = ContextCompat.getColor(requireContext(), R.color.light_red)
+        val currentId = firebaseAuth.currentUser?.uid
+        if (!currentId.isNullOrEmpty()) {
+            findMyChatroomMessage(currentId, badge)
+        }
+    }
+
+    private fun findMyChatroomMessage(currentId: String, badge: BadgeDrawable) {
+        firestore.collection(CHATROOMS_COLLECTION_PATH).whereArrayContains(USERS_FIELD, currentId)
+            .addSnapshotListener { snapshot, e ->
+                if (snapshot != null) {
+                    val count =
+                        getMessageCount(snapshot.toObjects(ChatRoom::class.java), currentId)
+                    setBadgeState(count, badge)
+                }
+            }
+    }
+
+    private fun setBadgeState(count: Int, badge: BadgeDrawable) {
+        badge.isVisible = if (count == 0) false else true
+        badge.number = if (count > MAX_MESSAGE_COUNT) MAX_MESSAGE_COUNT else count
+    }
+
+    private fun getMessageCount(chaRooms: List<ChatRoom>, currentId: String): Int {
+        var messageCount = 0
+        for (chatroom in chaRooms) {
+            messageCount += chatroom.unreadCount.get(currentId) ?: 0
+        }
+        return messageCount
+    }
+
+    companion object {
+        const val MAX_MESSAGE_COUNT = 999
     }
 }
